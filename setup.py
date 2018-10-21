6d6cd84da663c39009d3894977f3e61c88d0969b
Isaac C. Joseph <isaac.joseph4@agilent.com>
2018-10-21 14:01:55 -0700

    rm

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 setuptools.setup(name='isajosep_util',
                  version='0.2',
                  description="Misc utilities",                 
-                 author="Isaac Joseph", author_email="isaac.c.joseph@gmail.com",
+                 author="Isaac Joseph",
                  classifiers=['Programming Language :: Python :: 3.6',
                               'Programming Language :: Python :: 2.7'],
                  install_requires=['pandas'],
